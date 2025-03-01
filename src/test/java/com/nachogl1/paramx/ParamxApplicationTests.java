d02b09bcf710eaade1c12e7ac80eb09a84483bff
nachogl1 <i.gonzalezl@hotmail.com>
2025-03-01 20:52:01 +0000

    build: Updating way I use pre-commit hook

--- src/test/java/com/nachogl1/paramx/ParamxApplicationTests.java
+++ src/test/java/com/nachogl1/paramx/ParamxApplicationTests.java
@@ -3,11 +3,13 @@ package com.nachogl1.paramx;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Fail.fail;
+
 @SpringBootTest
 class ParamxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
